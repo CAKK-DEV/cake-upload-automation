59d87213fd53d15b1b655646ba04eb1b8477de21
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-07-02 18:53:15 +0900

    init project

---
+++ src/main/java/com/cakk/upload_cake/CakkApplication.java
@@ -0,0 +1,13 @@
+package com.cakk.upload_cake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cakk/upload_cake/CakkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakk.upload_cake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
